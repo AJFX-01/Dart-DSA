44278f0a58a8b3edb73d5d0d03103ad75a3cde54
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-09-17 14:56:34 +0100

    feat: n queens with backtrack

--- BackTracking/N-queens.dart
+++ BackTracking/N-queens.dart
@@ -1,3 +1,13 @@
 class NQueen {
-  
+  List<List<String>> solveNQueens(int n) {
+    List<List<String>> solutions = [];
+    List<int> queens = List.filled(n, -1);
+
+    Set<int> cols = {};
+    Set<int> diagonals1 = {};
+    Set<int> diagonals2 = {};
+
+    //create board
+    //bactrack
+  }
 }
