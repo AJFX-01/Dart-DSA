541bca398b1d4896f493aba638a429be7b656c00
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-09-19 03:06:15 +0100

    feta: both dfs and bfs

--- Trees/numberOfProvinvces.dart
+++ Trees/numberOfProvinvces.dart
@@ -1,30 +1,65 @@
-class NumberOfProvision {
+import 'dart:collection';
+
+class NumberOfProvisionDFS {
   int findCircleNum(List<List<int>> isConnected) {
     int n = isConnected.length;
-
     List<bool> visited = List.filled(n, false);
-
     int provinces = 0;
-
-    void dfs(int city) {
-      visited[city] = true;
-
-      for (int neighbor = 0; neighbor < n; neighbor++) {
-        // check if the city is connected and not visited yet, contiue dfs
-        if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
-          dfs(neighbor);
-        }
-      }
-    }
 
     // loop through the city and find connected components
     for (int city = 0; city < n; city++) {
       if (!visited[city]) {
-        dfs(city);
+        dfs(city, isConnected, visited);
         provinces++;
       }
     }
 
     return provinces;
+  }
+
+  void dfs(int city, List<List<int>> isConnected, List<bool> visited) {
+    visited[city] = true;
+
+    for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+      // check if the city is connected and not visited yet, contiue dfs
+      if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+        dfs(neighbor, isConnected, visited);
+      }
+    }
+  }
+}
+
+class NumberProvinceBFS {
+  int findCircleNum(List<List<int>> isConnected) {
+    int n = isConnected.length;
+    List<bool> visited = List.filled(n, false);
+    int provinces = 0;
+
+    // loop through the city and find connected components
+    for (int city = 0; city < n; city++) {
+      if (!visited[city]) {
+        bfs(isConnected, city, visited);
+        provinces++;
+      }
+    }
+
+    return provinces;
+  }
+
+  void bfs(List<List<int>> isConnected, int startCity, List<bool> visited) {
+    Queue<int> queue = Queue();
+    queue.add(startCity);
+
+    while (queue.isNotEmpty) {
+      int city = queue.removeFirst();
+      // check all cities connected to current city
+
+      for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+        if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.add(neighbor);
+        }
+      }
+    }
   }
 }
