8df2f1f46691c43e5869d21bfb24a5a2a133baae
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-09-19 04:13:52 +0100

    feat: done with the number of islands

--- Trees/numberOfIsland.dart
+++ Trees/numberOfIsland.dart
@@ -1,12 +1,43 @@
 class NumberOfIsland {
-  int numsIslands(List<List<int>> grid) {
+  int numsIslands(List<List<String>> grid) {
     if (grid.isEmpty || grid[0].isEmpty) return 0;
 
     int numRows = grid.length;
     int numCols = grid[0].length;
+    int numberOfIslands = 0;
 
-    
+    //Tranverse the entire grid
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        // if we find 1 its a new island
+        if (grid[i][j] == "1") {
+          // increment the count of islands
+          numberOfIslands++;
+
+          // perform dfs on the island
+          dfs(grid, j, i);
+        }
+      }
+    }
+
+    return numberOfIslands;
   }
 
-  void dfs() {}
+  void dfs(List<List<String>> grid, int col, int row) {
+    if (col < 0 ||
+        col >= grid[0].length ||
+        row < 0 ||
+        row >= grid.length ||
+        grid[row][col] == '0') {
+      return;
+    }
+
+    grid[row][col] = '0';
+
+    // peform df in all four directions
+    dfs(grid, col, row - 1); //up
+    dfs(grid, col, row + 1); //down
+    dfs(grid, col - 1, row); // left
+    dfs(grid, col + 1, row); // right
+  }
 }
