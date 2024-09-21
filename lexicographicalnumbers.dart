3e3b71b43c3efadc3d4dddc8f241a70fe283dc82
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-09-21 20:53:58 +0100

    feat: solved lexicographical order

---
+++ lexicographicalnumbers.dart
@@ -0,0 +1,23 @@
+class LexicoGraphicalOrderNums {
+  List<int> lexicalOrder(int n) {
+    List<int> result = [];
+
+    void dfs(int current) {
+      if (current > n) return;
+
+      result.add(current);
+      for (int i = 0; i < 9; i++) {
+        int next = current * 10 + i;
+        if (next > n) break;
+        dfs(next);
+      }
+    }
+
+    for (int num = 1; num < 9; num++) {
+      if (num > n) break;
+      dfs(num);
+    }
+
+    return result;
+  }
+}
