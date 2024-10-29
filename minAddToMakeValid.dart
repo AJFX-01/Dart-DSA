026e30d3cc4f7324cf9de37771d59ee196e45bac
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-10-29 04:56:18 +0100

    feat: made adjustment

--- minAddToMakeValid.dart
+++ minAddToMakeValid.dart
@@ -21,24 +21,3 @@ class MinAddToMakeValid {
     return open + close;
   }
 }
-
- List<int> stack = [];
-
-  // Build the stack of indices with decreasing values
-  for (int i = 0; i < nums.length; i++) {
-    if (stack.isEmpty || nums[stack.last] > nums[i]) {
-      stack.add(i);  // Store indices of decreasing values
-    }
-  }
-
-  int maxWidth = 0;
-  
-  // Traverse the array from right to left
-  for (int j = nums.length - 1; j >= 0; j--) {
-    // Try to find the largest possible width ramp
-    while (stack.isNotEmpty && nums[stack.last] <= nums[j]) {
-      maxWidth = max(maxWidth, j - stack.removeLast());
-    }
-  }
-
-  return maxWidth;
