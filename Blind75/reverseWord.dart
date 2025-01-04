8b159a146e1c905e8f9c0fe63f6e29db8300d351
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-01-04 18:36:47 +0100

    feat: solved reversed words

--- Blind75/reverseWord.dart
+++ Blind75/reverseWord.dart
@@ -5,13 +5,13 @@ class ReserveWord {
     List<String> newWords = s.trim().split(RegExp(r'\s+'));
 
     // reserve the string
-    for (int i = 0; j = newWords.length - 1; i < j; i++, j--) {
-      String temp = newwords[i]
-      newWords[i] = newWords[j]
+    for (int i = 0, j = newWords.length - 1; i < j; i++, j--) {
+      String temp = newWords[i];
+      newWords[i] = newWords[j];
       newWords[j] = temp; 
     }
 
-    return newWords.join(" ")
+    return newWords.join(" ");
   }
 }
 
