882edd2e63922b593c31c967635e31784ae44d81
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-02-04 15:09:13 +0100

    feat: completed

--- Blind75/productExceptSelf.dart
+++ Blind75/productExceptSelf.dart
@@ -1,3 +1,23 @@
 class Productexceptself {
-  
+  List<int> productExceptSelf(List<int> nums) {
+    int n = nums.length;
+    List<int> answer = List.filled(n, 1);
+
+    // compute the prefix sum
+    int prefixsum = 1;
+    int suffixsum = 1;
+
+    for (int i = 0; i < n; i++) {
+      answer[i] = prefixsum;
+      prefixsum *= nums[i];
+    }
+    // compute the suffix sum
+
+    for (int i = (n - 1); i > (n - 1); i--) {
+      answer[i] *= suffixsum;
+      suffixsum *= nums[i];
+    }
+
+    return answer;
+  }
 }
